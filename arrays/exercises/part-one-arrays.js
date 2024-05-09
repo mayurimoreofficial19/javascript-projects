9d600a03e6875defda5a45fee819a8bbf78855f3
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-05-09 10:04:05 -0500

    Arrays assignments are done!

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,24 @@
 //Create an array called practiceFile with the following entry: 273.15
 
+let practiceFile = [];
+
+console.log(practiceFile.push(42));
+console.log(practiceFile.push("hello"));
+console.log(practiceFile.push("false",-4.6,"87"));
+console.log(practiceFile);
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
 
+practiceFile[5] = "42";
+console.log(practiceFile);
+practiceFile[6] = "hello";
+console.log(practiceFile);
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+
+
+let practiceArray = [];
+
+console.log(practiceArray.push(-4.6));
+console.log(practiceArray);
+console.log(practiceArray.push(87));
+console.log(practiceArray);
+
