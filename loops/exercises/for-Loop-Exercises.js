038dc4737dc4562fa3fab3d178db35a97733e926
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-05-11 21:58:54 -0500

    for loop exercise done

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,94 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+//a...
 
+let number;
+let i;
+// for(i=0; i<=20; i++)
+//   {
+//     console.log(i);
+//   }
 
+//b....
 
+// for(i = 3; i < 30; i++)
+//   {
+//     if(i % 2 === 1)
+//       {
+//         console.log("Odd : " + i);
+//       }
+//   }
+
+//c.....
+
+// for(i = 12; i >= -14; i--)
+//   {
+//     if(i % 2 === 0)
+//       {
+//         console.log("Even : " + i);
+//       }
+//   }
+
+//d....
+
+// for (i = 50; i >= 20; i--) 
+//   { 
+//      if (i % 3 === 0) 
+//       { 
+//           console.log(i);
+//       }
+// } 
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
 
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+//a.....  
+let organizationName = "LaunchCode";
+let randomArray = [1, 5, "LC101", "blue", 42];
+let answer = "";
 
+// for(i = 0; i < randomArray.length; i++)
+//   {
+//     console.log(randomArray[i]);
+//   }
 
+//b.....
 
+// for(i = 0; i < organizationName.length; i++)
+//   {
+//       answer = organizationName[i] + answer;
+//   }
+//   console.log(answer);
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+let otherArr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [], odds = [];
+
+//Even Numbers
+for(i = 0; i < otherArr.length; i++)
+  {
+    if(otherArr[i] % 2 === 0)
+      {
+        evens = otherArr[i];
+        console.log("Even Numbers : " + evens);
+      }
+  }
+
+//Odd Numbers
+
+for(i = 0; i < otherArr.length; i++)
+  {
+    if(otherArr[i] % 2 != 0)
+      {
+        odds = otherArr[i];
+        console.log("Odd Numbers : " + odds);
+      }
+  }
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,14 +1,14 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
-
-
-
-
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-
-
+  const input = require('readline-sync');
+  let fuelLevel = 0, numAstronauts = 0, altitude = 0;
+  
+  while (fuelLevel <= 5000 || fuelLevel > 30000 || isNaN(fuelLevel)) {
+     fuelLevel = input.question("Enter the starting fuel level: ");
+  }
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
