c9d39ef1f831f8404c6e3681b362f057a5bdc322
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-06-18 22:20:24 -0500

    unit-testing chapter examples-completed

--- unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
+++ unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
@@ -1,5 +1,33 @@
-describe("transmission processor", function() {
+const processor = require("../processor.js");
 
-   //  TODO: put tests here
- 
- });
+describe("transmission processor", function () {
+  test("Takes a string and returns an Object", function () {
+    let result = processor("9701::<489584872710>");
+    expect(typeof result).toBe("object");
+  });
+  //  TODO: put tests here
+  test("returns -1 if '::' not found", function () {
+    let result = processor("9701<489584872710>");
+    expect(result).toBe(-1);
+  });
+
+  test("returns id in object", function () {
+    let result = processor("9701::<489584872710>");
+    expect(result.id).not.toBeUndefined();
+  });
+
+  test("converts id to a number", function () {
+    let result = processor("9701::<489584872710>");
+    expect(result.id).toBe(9701);
+  });
+
+  test("returns rawData in objects", function () {
+    let result = processor("9701::<489584872710>");
+    expect(result.rawData).not.toBeUndefined();
+  });
+
+  test("returns -1 for rawData if missing < at position 0", function () {
+    let result = processor("9701::489584872710>");
+    expect(result.rawData).toBe(-1);
+  });
+});
