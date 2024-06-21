8b53beac3f1941f7c07654cb603ce1441dd42591
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-06-20 20:48:56 -0500

    studio completed

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,32 @@
-
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (number) {},
+};
+
+function launchOutput(number) {
+  const divisibleBy2 = divisibleBy2 % 2 == 0;
+  const divisibleBy3 = divisibleBy3 % 3 == 0;
+  const divisibleBy5 = divisibleBy5 % 5 == 0;
+
+  if (divisibleBy2 && divisibleBy3 && divisibleBy5) {
+    return "LaunchCode Rocks!";
+  } else if (divisibleBy2 && divisibleBy3) {
+    return "LaunchCode!";
+  } else if (divisibleBy3 && divisibleBy5) {
+    return "Code Rocks!";
+  } else if (divisibleBy2 && divisibleBy5) {
+    return "Launch Rocks!";
+  } else if (divisibleBy2) {
+    return "Launch!";
+  } else if (divisibleBy3) {
+    return "Code!";
+  } else if (divisibleBy5) {
+    return "Rocks!";
+  } else {
+    return "Rutabagas! That doesn't work.";
+  }
 }
-
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,35 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const launchcode = require("../index.js");
 
-describe("Testing launchcode", function(){
-
+describe("Testing launchcode", function () {
   // Write your unit tests here!
-  
+  test("value of organization is non profit", function () {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("value of executive director is 'Jeff'", function () {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("value of percentageCoolEmployees is '100'", function () {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("value of programsOffered is '(Web Development, Data Analysis, Liftoff)'", function () {
+    let result = launchcode.programsOffered;
+    expect(result).toContain("Web Development");
+    expect(result).toContain("Data Analysis");
+    expect(result).toContain("Liftoff");
+    expect(result).toHaveLength(3);
+  });
+
+  test("returns 'Launch!' when passed a number that is only divisible by 2", function () {
+    expect(launchcode.launchOutput(4)).toBe("Launch!");
+    expect(launchcode.launchOutput(8)).toBe("Launch!");
+  });
+
+  it("return 'Code!' when passed a number that is only divisible by 3", function () {
+    expect(launchcode.launchOutput(9)).toBe("Code!");
+    expect(launchcode.launchOutput(15)).toBe("Code!");
+  });
 });
