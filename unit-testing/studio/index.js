5f981e871913c5115c528fa2c0cba52a3925d964
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-06-20 21:52:01 -0500

    Studio completed

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -3,30 +3,32 @@ let launchcode = {
   executiveDirector: "Jeff",
   percentageCoolEmployees: 100,
   programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
-  launchOutput: function (number) {},
+  launchOutput: function (number) {
+    let divisibleBy2;
+    divisibleBy2 = number % 2 == 0;
+    let divisibleBy3;
+    divisibleBy3 = number % 3 == 0;
+    let divisibleBy5;
+    divisibleBy5 = number % 5 == 0;
+
+    if (divisibleBy2 && divisibleBy3 && divisibleBy5) {
+      return "LaunchCode Rocks!";
+    } else if (divisibleBy2 && divisibleBy3) {
+      return "LaunchCode!";
+    } else if (divisibleBy3 && divisibleBy5) {
+      return "Code Rocks!";
+    } else if (divisibleBy2 && divisibleBy5) {
+      return "Launch Rocks!";
+    } else if (divisibleBy2) {
+      return "Launch!";
+    } else if (divisibleBy3) {
+      return "Code!";
+    } else if (divisibleBy5) {
+      return "Rocks!";
+    } else {
+      return "Rutabagas! That doesn't work.";
+    }
+  },
 };
 
-function launchOutput(number) {
-  const divisibleBy2 = divisibleBy2 % 2 == 0;
-  const divisibleBy3 = divisibleBy3 % 3 == 0;
-  const divisibleBy5 = divisibleBy5 % 5 == 0;
-
-  if (divisibleBy2 && divisibleBy3 && divisibleBy5) {
-    return "LaunchCode Rocks!";
-  } else if (divisibleBy2 && divisibleBy3) {
-    return "LaunchCode!";
-  } else if (divisibleBy3 && divisibleBy5) {
-    return "Code Rocks!";
-  } else if (divisibleBy2 && divisibleBy5) {
-    return "Launch Rocks!";
-  } else if (divisibleBy2) {
-    return "Launch!";
-  } else if (divisibleBy3) {
-    return "Code!";
-  } else if (divisibleBy5) {
-    return "Rocks!";
-  } else {
-    return "Rutabagas! That doesn't work.";
-  }
-}
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -23,13 +23,34 @@ describe("Testing launchcode", function () {
     expect(result).toHaveLength(3);
   });
 
+  test("returns 'Rutabagas! That doesn't work.' when passed a number that is not divisible by 2, 3 or 5", function () {
+    expect(launchcode.launchOutput(37)).toBe("Rutabagas! That doesn't work.");
+  });
+
+  test("returns 'LaunchCode Rocks!' when passed a number that is divisible by 2, 3 or 5", function () {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+  });
+
+  test("returns 'Launch Rocks!' when a number that is divisible by 2 AND 5, return", function () {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks!");
+  });
+
+  test("returns 'Code Rocks!' when a number that is divisible by 3 AND 5", function () {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  });
+
+  test("returns 'LaunchCode!' when a number that is divisible by 2 AND 3", function () {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  });
+
+  test("retuns 'Rocks!' when a number that is ONLY divisible by 5", function () {
+    expect(launchcode.launchOutput(25)).toBe("Rocks!");
+  });
   test("returns 'Launch!' when passed a number that is only divisible by 2", function () {
     expect(launchcode.launchOutput(4)).toBe("Launch!");
-    expect(launchcode.launchOutput(8)).toBe("Launch!");
   });
 
   it("return 'Code!' when passed a number that is only divisible by 3", function () {
     expect(launchcode.launchOutput(9)).toBe("Code!");
-    expect(launchcode.launchOutput(15)).toBe("Code!");
   });
 });
