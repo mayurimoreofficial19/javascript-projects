ea9838926b2d19a1ee1d6db489ce9902ae7437fa
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-06-22 00:13:45 -0500

    exception exercises completed

--- exceptions/chapter-examples/throw-default-error.js
+++ exceptions/chapter-examples/throw-default-error.js
@@ -1,1 +1,13 @@
-throw Error('You cannot divide by zero!');
+function division(x, y) {
+  if (y == 0) {
+    throw new Error("You cannot divide by zero!");
+  }
+  return x / y;
+}
+
+try {
+  console.log(division(10, 5));
+  console.log(division(3, 0));
+} catch (err) {
+  console.log("Error Occurred : " + err.message);
+}
