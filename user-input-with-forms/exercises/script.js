80d01cc685e634fcca063ac1e03be7ace5f8bf1b
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-08-16 18:37:19 -0500

    user-input-with-form-exercise-completed

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,12 @@
 //Code Your Solution Below
+<script>
+        document
+          .getElementById("testForm")
+          .addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const testName = document.getElementById("testName").value;
+            console.log("Submitted testName:", testName);
+          });
+      </script>
+      
